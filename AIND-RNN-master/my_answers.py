4a39fa6e40ff964879d9ef10ba28bb8a26ef32cd
Mary Troiani <troiani.mary@gmail.com>
2017-07-28 21:16:47 -0700

    feat: completed section 2.

--- AIND-RNN-master/my_answers.py
+++ AIND-RNN-master/my_answers.py
@@ -1,12 +1,14 @@
 import numpy as np
+import re
 
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import LSTM
+from keras.layers import Activation
 import keras
 
 
-# TODO: fill out the function below that transforms the input series 
+# TODO: fill out the function below that transforms the input series
 # and window-size into a set of input/output pairs for use with our RNN model
 def window_transform_series(series, window_size):
     # containers for input/output pairs
@@ -35,6 +37,11 @@ def build_part1_RNN(window_size):
 ### TODO: return the text input with only ascii lowercase and the punctuation given below included.
 def cleaned_text(text):
     punctuation = ['!', ',', '.', ':', ';', '?']
+    accents = {'é': 'e', 'è': 'e', 'â': 'a', 'à': 'a'}
+    for k,v in accents.items():
+        text = text.replace(k, v)
+
+    text = re.sub("[^a-z !,.:;?]", " ", text)
 
     return text
 
@@ -44,9 +51,19 @@ def window_transform_text(text, window_size, step_size):
     inputs = []
     outputs = []
 
+    for i in range(0, len(text) - window_size, step_size):
+        inputs.append(text[i:i+window_size])
+        outputs.append(text[i+window_size:i+window_size+1])
+
     return inputs,outputs
 
 # TODO build the required RNN model: 
 # a single LSTM hidden layer with softmax activation, categorical_crossentropy loss 
 def build_part2_RNN(window_size, num_chars):
-    pass
+    model = Sequential()
+    model.add(LSTM(200, input_shape=(window_size,num_chars)))
+    model.add(Dense(num_chars))
+    model.add(Activation('softmax'))
+
+    return model
+
